f35191e2a8c5158c702b1dd2f7e983171e3521c8
Oleg <oleg.rabyshko@mail.ru>
2021-10-06 16:39:09 +0300

    feat: added tests

--- pointing-poker/src/tests/App.test.tsx
+++ pointing-poker/src/tests/App.test.tsx
@@ -1,7 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
+import { store } from '../redux';
 
-it('test', () => {
-  render(<App />);
-  screen.debug();
+describe('App Component', () => {
+  const el = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+  );
+  it('App  exist', () => {
+    expect(el).not.toBeNull();
+  });
+
+  it('App renders without crashing', () => {
+    const root = document.createElement('div');
+    ReactDOM.render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>,
+      root,
+    );
+    ReactDOM.unmountComponentAtNode(root);
+  });
 });
